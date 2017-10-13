193a4997420623c734300fff743630d676424d42
Peter Rasmussen <leonhardt_88@live.dk>
2017-10-13 10:03:11 +0200

    first commit, node_modules excluded

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./MyNewApp"
+import Converter from "./Converter"
+
+
+//const root = document.getElementById("root");
+
+ReactDOM.render(<App />, document.getElementById('root'));
